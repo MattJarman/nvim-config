ae3cb03af3d689fd152ce9a0580365f5a03199ba
Matthew Jarman <37220140+MattJarman@users.noreply.github.com>
2024-10-27 21:23:12 +0000

    Add files via upload

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,6 +1,18 @@
 return {
   "folke/noice.nvim",
   opts = {
+    routes = {
+      {
+        filter = {
+          event = "notify",
+          kind = "info",
+          any = {
+            { find = "hidden" },
+          },
+        },
+        opts = { skip = true },
+      },
+    },
     lsp = {
       hover = {
         silent = true,
