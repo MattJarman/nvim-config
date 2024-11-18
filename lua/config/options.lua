976390ac0b26045b9969a82cb65061c0554c25c5
Matthew Jarman <37220140+MattJarman@users.noreply.github.com>
2024-11-18 11:42:48 +0000

    Update options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.o.timeout = false
+vim.g.traces_abolish_integration = 1
