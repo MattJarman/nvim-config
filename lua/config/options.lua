8c1767d03fcb487de188ed71bd5d6a70b239f79c
Matthew Jarman <37220140+MattJarman@users.noreply.github.com>
2024-12-20 12:05:29 +0000

    Update options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.o.timeout = false
+-- vim.o.timeout = false
 vim.g.traces_abolish_integration = 1
