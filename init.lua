e407de2ecfc43d4dad7a990917c34c95bc648667
Matthew Jarman <37220140+MattJarman@users.noreply.github.com>
2025-03-25 15:13:02 +0000

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- macros
+vim.fn.setreg("l", vim.api.nvim_replace_termcodes('yoconsole.log("<Esc>pa: ", <Esc>p$a;<Esc>"', true, true, true))
