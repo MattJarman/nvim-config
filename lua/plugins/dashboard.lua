f52dc8fbbbb2435eb8fb4506dba0f5d27b9c16ac
Matt Jarman <matt@mac.local>
2025-04-01 01:21:03 +0100

    chore: refactor dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,37 +1,11 @@
 return {
-  {
-    "folke/snacks.nvim",
-    opts = {
-      dashboard = {
-        enabled = true,
-        preset = {
-          ---@type snacks.dashboard.Item[]|fun(items:snacks.dashboard.Item[]):snacks.dashboard.Item[]?
-          keys = {
-            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-            {
-              icon = " ",
-              key = "g",
-              desc = "Find Text",
-              action = ":lua Snacks.dashboard.pick('live_grep')",
-            },
-            {
-              icon = " ",
-              key = "r",
-              desc = "Recent Files",
-              action = ":lua Snacks.dashboard.pick('oldfiles')",
-            },
-            {
-              icon = " ",
-              key = "c",
-              desc = "Config",
-              action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-            },
-            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy },
-            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-          },
-          header = [[
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    dashboard = {
+      preset = {
+        header = [[
                                                                        
                                                                      
        ████ ██████           █████      ██                     
@@ -42,72 +16,64 @@ return {
   ███████████ ███    ███ █████████ █████ █████ ████ █████  
  ██████  █████████████████████ ████ █████ █████ ████ ██████ 
                                                                        
-          ]],
+        ]],
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          {
+            icon = " ",
+            key = "g",
+            desc = "Find Text",
+            action = ":lua Snacks.dashboard.pick('live_grep')",
+          },
+          {
+            icon = " ",
+            key = "r",
+            desc = "Recent Files",
+            action = ":lua Snacks.dashboard.pick('oldfiles')",
+          },
+
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
-        formats = {
-          terminal = { align = "center" },
-          version = { "%s", align = "center" },
-        },
-        sections = {
-          { section = "header" },
-          { section = "keys", gap = 1, padding = 1 },
-          { section = "startup" },
-          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-          function()
-            local in_git = Snacks.git.get_root() ~= nil
-            local cmds = {
-              {
-                title = "Notifications",
-                cmd = "gh notify -s -a -n5",
-                action = function()
-                  vim.ui.open("https://github.com/notifications")
-                end,
-                enabled = function()
-                  return vim.fn.executable("gh") == 1
-                end,
-                key = "n",
-                icon = " ",
-                height = 5,
-                indent = 2,
-                padding = 1,
-              },
-              {
-                icon = " ",
-                title = "Open PRs",
-                cmd = "gh pr list -L 3",
-                key = "P",
-                enabled = function()
-                  return vim.fn.executable("gh") == 1
-                end,
-                action = function()
-                  vim.fn.jobstart("gh pr list --web", { detach = true })
-                end,
-                height = 7,
-                indent = 2,
-                padding = 1,
-              },
-              {
-                icon = " ",
-                title = "Git Status",
-                cmd = "git --no-pager diff --stat -B -M -C",
-                height = 10,
-                indent = 2,
-                padding = 1,
-              },
-            }
-            return vim.tbl_map(function(cmd)
-              return vim.tbl_extend("force", {
-                pane = 2,
-                section = "terminal",
-                enabled = function()
-                  return in_git and vim.o.columns > 130
-                end,
-                padding = 1,
-                -- ttl = 5 * 60,
-              }, cmd)
-            end, cmds)
+      },
+
+      sections = {
+        { section = "header" },
+        { section = "keys", gap = 1, padding = 2 },
+        {
+          icon = " ",
+          title = "Recent Files",
+          section = "recent_files",
+          indent = 2,
+          padding = 2,
+          enabled = function()
+            return vim.o.lines > 40
           end,
         },
+        function()
+          local in_git = Snacks.git.get_root() ~= nil
+          local cmds = {
+            {
+              icon = " ",
+              title = "Git Status",
+              cmd = "git --no-pager diff --stat -B -M -C",
+              height = 10,
+            },
+          }
+          return vim.tbl_map(function(cmd)
+            return vim.tbl_extend("force", {
+              section = "terminal",
+              padding = 1,
+              ttl = 5 * 60,
+              indent = 2,
+              enabled = function()
+                return in_git and vim.o.lines > 60
+              end,
+            }, cmd)
+          end, cmds)
+        end,
+        { section = "startup", padding = 1 },
       },
     },
   },
