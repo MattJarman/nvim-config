b2a561a7ed48f51a2b22d5a13a9b7732b1e64efc
Matthew Jarman <37220140+MattJarman@users.noreply.github.com>
2026-07-04 20:32:30 +0100

    Update colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,8 @@
 return {
-  { "Mofiqul/dracula.nvim" },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
   {
     "rebelot/kanagawa.nvim",
     opts = {
+      transparent = true,
       colors = {
         theme = {
           all = {
@@ -18,6 +12,30 @@ return {
           },
         },
       },
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          -- Floating windows (LSP hover, completion docs, etc.)
+          NormalFloat = { bg = "none" },
+          FloatBorder = { bg = "none" },
+          FloatTitle = { bg = "none" },
+
+          -- Save an hlgroup with dark background and dimmed foreground
+          -- so that you can use it where your parent bg is transparent
+          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+          -- Popup menu (completion) with transparency
+          Pmenu = { fg = theme.ui.shade0, bg = "none" },
+          PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+          PmenuSbar = { bg = theme.ui.bg_m1 },
+          PmenuThumb = { bg = theme.ui.bg_p2 },
+
+          -- Statusline / command area transparency
+          StatusLine = { bg = "none" },
+          StatusLineNC = { bg = "none" },
+          MsgArea = { bg = "none" },
+        }
+      end,
     },
   },
   {
