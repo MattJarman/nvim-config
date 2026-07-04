2a9a80b91bbe52e7d5c33e604ed1f34dba0c3937
Matthew Jarman <37220140+MattJarman@users.noreply.github.com>
2026-07-04 20:32:02 +0100

    Update coding.lua

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -12,7 +12,7 @@ return {
     },
   },
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = { ensure_installed = { "emmet-language-server" } },
   },
   { "tpope/vim-abolish" },
